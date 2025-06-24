7389e1388a963ed653a6057052eee03f4cadf147
Viacheslav Poturaev <nanopeni@gmail.com>
2025-06-25 00:51:34 +0200

    Update dbcon.rb

--- dbcon.rb
+++ dbcon.rb
@@ -1,16 +1,16 @@
 class Dbcon < Formula
   desc "dbcon is a web-based AI-enabled SQL console tailored for sharing and reporting"
   homepage "https://github.com/vearutop/dbcon"
-  version "0.4.0"
+  version "0.4.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/vearutop/dbcon/releases/download/v0.4.0/darwin_arm64.tar.gz"
-      sha256 "32c04d58f2e3ead1324620a7860e03f8264d7bbc2b398c7c2370fc41276c9699"
+      url "https://github.com/vearutop/dbcon/releases/download/v0.4.2/darwin_arm64.tar.gz"
+      sha256 "a5c2501b198d0cd9717a09503c9f049f5cfd2b9719aff200c1f85850907d1005"
     end
     on_intel do
-      url "https://github.com/vearutop/dbcon/releases/download/v0.4.0/darwin_amd64.tar.gz"
-      sha256 "9ee59561ac037ade7adc42a2decd894b8367d78608adac645887e2080920d3c3"
+      url "https://github.com/vearutop/dbcon/releases/download/v0.4.2/darwin_amd64.tar.gz"
+      sha256 "e7603dff110ed0a3b7df3765d3e44caf096bfe780be0513123f095842da73984"
     end
   end
 
